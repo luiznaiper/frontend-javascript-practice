7f5642307a1339bed9098be12b14ed13495a4ffa
luiznaiper <luiz_naiper@hotmail.com>
2022-08-10 19:43:51 -0500

    :construction: Add mobile menu

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const $ = (selector) => document.querySelector(selector);
 const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
+const burgerMenu = $('.burger-menu');
+const mobileMenu = $('.mobile-menu');
 
 const toggleDesktopMenu = () => {
   desktopMenu.classList.toggle('inactive');
 };
 
+const toggleDesktopMobile = () => {
+  mobileMenu.classList.toggle('inactive');
+};
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleDesktopMobile);
