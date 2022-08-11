968e75bed4e13fa81293a32401549404eb035964
luiznaiper <luiz_naiper@hotmail.com>
2022-08-11 07:19:58 -0500

    :construction: Add toggle functions to disapear navs and menus

--- src/index.js
+++ src/index.js
@@ -3,14 +3,40 @@ const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 const burgerMenu = $('.burger-menu');
 const mobileMenu = $('.mobile-menu');
+const cartMenu = $('.navbar-shopping-cart');
+const asideCart = $('.product-detail');
 
 const toggleDesktopMenu = () => {
+  const isAsideOpen = !asideCart.classList.contains('inactive');
+
+  if (isAsideOpen) {
+    asideCart.classList.add('inactive');
+  }
   desktopMenu.classList.toggle('inactive');
 };
 
-const toggleDesktopMobile = () => {
+const toggleMenuMobile = () => {
+  const isAsideOpen = !asideCart.classList.contains('inactive');
+
+  if (isAsideOpen) {
+    asideCart.classList.add('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
 };
 
+const toggleCartMenu = () => {
+  const isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+  const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+  if (isMobileMenuOpen) {
+    mobileMenu.classList.add('inactive');
+  }
+  if (isDesktopMenuOpen) {
+    desktopMenu.classList.add('inactive');
+  }
+  asideCart.classList.toggle('inactive');
+};
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
-burgerMenu.addEventListener('click', toggleDesktopMobile);
+burgerMenu.addEventListener('click', toggleMenuMobile);
+cartMenu.addEventListener('click', toggleCartMenu);
